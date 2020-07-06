c9c5ef29afbc5d7777a246f20faa4823a4b3fc3b
Crew Spence <csspence123@gmail.com>
2020-07-06 05:39:17 -0500

    Wrote working solution.

--- power_udemy.js
+++ power_udemy.js
@@ -5,8 +5,23 @@ Power
 Write a function called power which accepts a base and an exponent.  The function should return the power of the base to the exponent.  The function should mimic the functionality of Math.pow() - do not worry aout negative bases and exponents.
 */
 
-function power(){
-    
+const power = (base, expo) => {
+  let counter = expo;
+  let total = base;
+  const innerFunc = () => {
+    total *= base;
+    counter--;
+    if(counter > 1) {
+      return innerFunc();
+    }
+  }
+  if(expo === 0) {
+    return 1;
+  }
+  if(counter > 1) {
+    innerFunc();
+  }
+  return total;
 }
 
 // power(2,0) // 1
