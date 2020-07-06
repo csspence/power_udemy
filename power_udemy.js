3544a3d66efcef3de359e9a4f85ebac6179a0152
Crew Spence <csspence123@gmail.com>
2020-07-06 05:31:14 -0500

    Created function stub for toy problem.

---
+++ power_udemy.js
@@ -0,0 +1,14 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+
+Power
+Write a function called power which accepts a base and an exponent.  The function should return the power of the base to the exponent.  The function should mimic the functionality of Math.pow() - do not worry aout negative bases and exponents.
+*/
+
+function power(){
+    
+}
+
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
